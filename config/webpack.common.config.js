d3a0b8bc834d0a22e3339fdd62858988255935a8
Vishal <vishal.reply@gmail.com>
2019-10-09 20:33:40 +0530

    Initial Commit

---
+++ config/webpack.common.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+    entry: {
+        main: './src/js/index.js'
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname , '../dist')
+    },
+    module:{
+        rules: [{
+        test: [/.js$/],
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets:[
+                    '@babel/preset-env'
+                ]
+            }
+        }
+    },{
+        test: [/.css$|.scss$/],
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+        ]
+    },{
+        test: /\.(png|jpg|svg|gif)$/,
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/images'
+            }
+        }
+    }]}
+    ,
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack 4 Starter",
+            template: "./src/index.html",
+            inject: true,
+            minify:{
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[chunkhash].css'
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/assets/images',
+            to: 'assets/images'
+        }]),
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.js','.ts']
+    }
+};
